e1181ab6065cf4c5bd61ed270978b8af88c1963b
aburto22 <aburto22@gmail.com>
2022-12-07 16:32:53 +0100

    created template day 8

---
+++ d8/lib.ts
@@ -0,0 +1,9 @@
+export const myFunctionTask1 = (input: string) => {
+  const arr = input.split("\n").map((line) => line.trim());
+  return input;
+};
+
+export const myFunctionTask2 = (input: string) => {
+  const arr = input.split("\n").map((line) => line.trim());
+  return input;
+};
