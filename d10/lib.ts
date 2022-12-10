36bf1515254495908b7639cf3a8a72f3385811cb
aburto22 <aburto22@gmail.com>
2022-12-10 09:24:00 +0100

    completed day 10

--- d10/lib.ts
+++ d10/lib.ts
@@ -1,9 +1,66 @@
-export const myFunctionTask1 = (input: string) => {
-  const arr = input.split("\n").map((line) => line.trim());
-  return input;
+const getCycles = (arr: string[]) => {
+  const cycles: number[] = [1];
+  arr.forEach((line) => {
+    const lastCycle = cycles.at(-1) as number;
+    if (line === "noop") {
+      cycles.push(lastCycle);
+      return;
+    }
+    const num = Number(line.split(" ")[1]);
+    cycles.push(lastCycle);
+    cycles.push(lastCycle + num);
+  });
+  return cycles;
 };
 
-export const myFunctionTask2 = (input: string) => {
+const getCycleStrenght = (cycles: number[]) => {
+  const target = [20, 60, 100, 140, 180, 220];
+  return target.map((index) => [index, cycles[index - 1]]);
+};
+
+export const getSignalStrenghts = (input: string) => {
   const arr = input.split("\n").map((line) => line.trim());
-  return input;
+  const cycles = getCycles(arr);
+  const strengths = getCycleStrenght(cycles);
+  const signalStrenghts = strengths.map(([inters, cycle]) => inters * cycle);
+
+  return signalStrenghts.reduce((sum, num) => sum + num, 0);
+};
+
+const getSplitCycles = (cycles: number[]): number[][] => {
+  const splitCycles: number[][] = [];
+
+  let counter = 40;
+
+  while (counter <= cycles.length) {
+    splitCycles.push(cycles.slice(counter - 40, counter));
+    counter += 40;
+  }
+
+  return splitCycles;
+};
+
+const getSprite = (cycles: number[]) => {
+  const CRT: string[] = [];
+  const splitCycles = getSplitCycles(cycles);
+
+  splitCycles.map((cycles) => {
+    let lineCRT = "";
+    cycles.forEach((cycle, i) => {
+      if (cycle - 1 === i || cycle === i || cycle + 1 === i) {
+        lineCRT += "#";
+        return;
+      }
+      lineCRT += ".";
+    });
+    CRT.push(lineCRT);
+  });
+  return CRT;
+};
+
+export const getLetters = (input: string) => {
+  const arr = input.split("\n").map((line) => line.trim());
+  const cycles = getCycles(arr);
+  const sprite = getSprite(cycles);
+  return sprite.join("\n");
 };
--- d10/test.ts
+++ d10/test.ts
@@ -1,17 +1,167 @@
-import { myFunctionTask1, myFunctionTask2 } from "./lib";
+import { getSignalStrenghts, getLetters } from "./lib";
 
 describe("Advent of code - day 4", () => {
-  const input = ``;
+  const input = `addx 15
+  addx -11
+  addx 6
+  addx -3
+  addx 5
+  addx -1
+  addx -8
+  addx 13
+  addx 4
+  noop
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx -35
+  addx 1
+  addx 24
+  addx -19
+  addx 1
+  addx 16
+  addx -11
+  noop
+  noop
+  addx 21
+  addx -15
+  noop
+  noop
+  addx -3
+  addx 9
+  addx 1
+  addx -3
+  addx 8
+  addx 1
+  addx 5
+  noop
+  noop
+  noop
+  noop
+  noop
+  addx -36
+  noop
+  addx 1
+  addx 7
+  noop
+  noop
+  noop
+  addx 2
+  addx 6
+  noop
+  noop
+  noop
+  noop
+  noop
+  addx 1
+  noop
+  noop
+  addx 7
+  addx 1
+  noop
+  addx -13
+  addx 13
+  addx 7
+  noop
+  addx 1
+  addx -33
+  noop
+  noop
+  noop
+  addx 2
+  noop
+  noop
+  noop
+  addx 8
+  noop
+  addx -1
+  addx 2
+  addx 1
+  noop
+  addx 17
+  addx -9
+  addx 1
+  addx 1
+  addx -3
+  addx 11
+  noop
+  noop
+  addx 1
+  noop
+  addx 1
+  noop
+  noop
+  addx -13
+  addx -19
+  addx 1
+  addx 3
+  addx 26
+  addx -30
+  addx 12
+  addx -1
+  addx 3
+  addx 1
+  noop
+  noop
+  noop
+  addx -9
+  addx 18
+  addx 1
+  addx 2
+  noop
+  noop
+  addx 9
+  noop
+  noop
+  noop
+  addx -1
+  addx 2
+  addx -37
+  addx 1
+  addx 3
+  noop
+  addx 15
+  addx -21
+  addx 22
+  addx -6
+  addx 1
+  noop
+  addx 2
+  addx 1
+  noop
+  addx -10
+  noop
+  noop
+  addx 20
+  addx 1
+  addx 2
+  addx 2
+  addx -6
+  addx -11
+  noop
+  noop
+  noop`;
 
-  it("Task 1 - should return correct total ", () => {
-    const actual = myFunctionTask1(input);
-    const expected = input;
+  it("Task 1 - should return correct total signal strenghts", () => {
+    const actual = getSignalStrenghts(input);
+    const expected = 13140;
 
     expect(actual).toEqual(expected);
   });
-  it("Task 2 - should return correct total ", () => {
-    const actual = myFunctionTask2(input);
-    const expected = input;
+  it.only("Task 2 - should return correct total ", () => {
+    const actual = getLetters(input);
+    const expected = `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`;
 
     expect(actual).toEqual(expected);
   });
