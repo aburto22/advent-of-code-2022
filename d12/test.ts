7bf95db5409a5e05b2edfade9e63c34274dc0c5d
aburto22 <aburto22@gmail.com>
2022-12-11 17:59:06 +0100

    added template d13

---
+++ d12/test.ts
@@ -0,0 +1,18 @@
+import { myFunctionTask1, myFunctionTask2 } from "./lib";
+
+describe("Advent of code - day 4", () => {
+  const input = ``;
+
+  it("Task 1 - should return correct total ", () => {
+    const actual = myFunctionTask1(input);
+    const expected = input;
+
+    expect(actual).toEqual(expected);
+  });
+  it("Task 2 - should return correct total ", () => {
+    const actual = myFunctionTask2(input);
+    const expected = input;
+
+    expect(actual).toEqual(expected);
+  });
+});
